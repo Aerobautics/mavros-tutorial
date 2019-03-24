919db939b0dc95dd864d3ec0a7c5c58d145e46ba
Aerobautics <48452349+Aerobautics@users.noreply.github.com>
2019-03-23 18:21:21 -0700

    Revised launch instructions
    Added revised launch instructions in the form of a shell script.

---
+++ posix_launcher.sh
@@ -0,0 +1,7 @@
+cd catkin_ws/src/Firmware/
+source /opt/ros/kinetic/setup.bash 
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/posix_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
+roslaunch px4 mavros_posix_sitl.launch
+
