79b3c167d978b3235bc2968e8dedbba5037aebd2
Aerobautics <48452349+Aerobautics@users.noreply.github.com>
2019-04-02 23:34:57 -0700

    Added image capture
    Added video feed capture using cv_bridge.

--- offboard_imaging.cpp
+++ offboard_imaging.cpp
@@ -1,23 +1,44 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include "ros/ros.h"
-#include "sensor_msgs/Image.h"
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
 
-void rawImageCallback(const sensor_msgs::Image::ConstPtr& msg) {
+static const std::string WINDOW_NAME = "Offboard Imaging";
 
+void rawImageCallback(const sensor_msgs::ImageConstPtr& msg) {
+	cv_bridge::CvImagePtr cvImagePtr;
+
+	try {
+		cvImagePtr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+	} catch(cv_bridge::Exception& e) {
+		ROS_ERROR("cv_bridge exception: %s", e.what());
+		return;
+	}
+	cv::imshow(WINDOW_NAME, cvImagePtr->image);
+	cv::waitKey(3);
 }
 
 int main(int argc, char *argv[]) {
 	ros::init(argc, argv, "offboard_imaging_node");
 	ros::NodeHandle nodeHandle;
-	ros::Subscriber imageSubscriber;
-	cv::namedWindow("Offboard Imaging", cv::WINDOW_AUTOSIZE);
+	ros::Subscriber subscriber;
+	image_transport::ImageTransport imageTransport(nodeHandle);
+	image_transport::Subscriber imageSubscriber;
+	image_transport::Publisher imagePublisher;
+	cv::namedWindow(WINDOW_NAME, cv::WINDOW_AUTOSIZE);
 	cv::VideoCapture capture;
 	cv::Mat frame;
 
-	imageSubscriber = nodeHandle.subscribe<sensor_msgs::Image>("image_raw", 32, &rawImageCallback);
+	imageSubscriber = imageTransport.subscribe("image_raw", 1, &rawImageCallback);
+	//imagePublisher = imageTransport.advertise("/image_converter/output_video", 1);
+
+	//subscriber = nodeHandle.subscribe<sensor_msgs::Image>("image_raw", 32, &rawImageCallback);
 	
 	ros::spin();
+	cv::destroyWindow(WINDOW_NAME);
 	
 	return 0;
 }
